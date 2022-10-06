31912077bafee7db96c355633d235db3f1d57ae6
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-06 20:18:24 +0200

    DONE, exercise 4

--- iteración-4.js
+++ iteración-4.js
@@ -10,3 +10,11 @@ const alien = {
     planet: 'Eden',
     weight: '259kg'
 }
+
+for (const key in alien) {
+    if (alien.hasOwnProperty.call(alien, key)) {
+        const element = alien[key];
+        
+    }
+    console.log(alien);
+}
